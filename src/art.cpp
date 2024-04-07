4e1ecd6637f9483432a6b72296fb0487aa543124
James51332 <jamesddadabo@gmail.com>
2024-04-07 15:11:55 -0500

    fourier epicycles

--- src/art.cpp
+++ src/art.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 #include "core/Input.h"
 
+#include "complex.h"
 #include "transform.h"
 
 namespace fft
@@ -14,35 +15,7 @@ art::art()
 {
   uiRenderer = new Vision::ImGuiRenderer(m_DisplayWidth, m_DisplayHeight, m_DisplayScale);
   renderer = new Vision::Renderer2D(m_DisplayWidth, m_DisplayHeight, m_DisplayScale);
-  camera = new Vision::OrthoCamera(m_DisplayWidth, m_DisplayHeight, 5.0f);
-
-  // ----- DFT and IDFT Test -----
-  std::vector<glm::vec2> times = { {1.0f, 0.0f}, {2.0f, 0.0f}, {3.0f, 0.0f} };
-  
-  cout << "Performing the DFT on the following time domain values:" << endl;
-  for (auto time : times)
-  {
-    cout << time.x << " + " << time.y << "i" << endl;
-  }
-  cout << "-------------------------" << endl;
-
-  std::vector<glm::vec2> freqs = transform::dft(times);
-
-  cout << "The resulting amplitudes in the frequency domain are:" << endl;
-  for (auto freq : freqs)
-  {
-    cout << freq.x << " + " << freq.y << "i" << endl;
-  }
-  cout << "-------------------------" << endl;
-
-  times = transform::idft(freqs);
-
-  cout << "The time domain values from the IDFT are:" << endl;
-  for (auto time : times)
-  {
-    cout << time.x << " + " << time.y << "i" << endl;
-  }
-  cout << "-------------------------" << endl;
+  camera = new Vision::OrthoCamera(m_DisplayWidth, m_DisplayHeight, 15.0f);
 }
 
 art::~art()
@@ -57,12 +30,24 @@ void art::OnUpdate(float timestep)
   glClearColor(0.1f, 0.8f, 0.3f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
 
+  // camera
+  if (Vision::Input::KeyDown(SDL_SCANCODE_P))
+    camera->Update(timestep);
+
   // render lines
   renderer->Begin(camera);
   for (int i = 0; i < positions.size(); i++)
   {
-    renderer->DrawLine(positions[i], positions[(i + 1) % positions.size()], glm::vec4(1.0f), 0.5f);
+    renderer->DrawPoint(positions[i], glm::vec4(0.5f, 0.5f, 0.8f, 1.0f), 0.2f);
   }
+
+  for (int i = 0; i < static_cast<int>(tracePoints.size()) - 1; i++)
+  {
+    renderer->DrawLine(tracePoints[i], tracePoints[(i + 1)], glm::vec4(1.0f), 0.1f);
+  }
+
+  static int maxCircles = 50;
+  ShowFFT(freqs, SDL_GetTicks() * 2.0f * M_PI / 10000.0f, maxCircles);
   renderer->End();
 
   // render ui
@@ -70,16 +55,39 @@ void art::OnUpdate(float timestep)
   if (ImGui::Begin("Settings"))
   {
     if (ImGui::Button("Clear Points (c)") || Vision::Input::KeyPress(SDL_SCANCODE_C))
+    {
       positions.clear();
+      freqs.clear();
+      tracePoints.clear();
+    }
+
+    if (ImGui::Button("Clear Trace (t)") || Vision::Input::KeyPress(SDL_SCANCODE_T))
+    {
+      tracePoints.clear();
+    }
+
+    if (ImGui::Button("Stop DFT (d)") || Vision::Input::KeyDown(SDL_SCANCODE_D))
+    {
+      freqs.clear();
+    }
+
+    if (ImGui::Button("Run DFT (space)") || Vision::Input::KeyPress(SDL_SCANCODE_SPACE))
+    {
+      tracePoints.clear();
+      freqs = transform::dft(positions);
+    }
+
+    if (ImGui::DragInt("Max Circles", &maxCircles, 1.0, 1.0, 100.0))
+    {
+      tracePoints.clear();
+    }
   }
   ImGui::End();
   uiRenderer->End();
 
   // add points
-  if (Vision::Input::MouseDown(SDL_BUTTON_LEFT))
-  {
+  if (Vision::Input::MousePress(SDL_BUTTON_LEFT) && !Vision::Input::KeyDown(SDL_SCANCODE_P))
     positions.push_back(camera->GetMouseInWorldSpace());
-  }
 }
 
 void art::OnResize()
@@ -87,6 +95,41 @@ void art::OnResize()
   uiRenderer->Resize(m_DisplayWidth, m_DisplayHeight);
   renderer->Resize(m_DisplayWidth, m_DisplayHeight);
   camera->SetWindowSize(m_DisplayWidth, m_DisplayHeight);
+}
+
+void art::ShowFFT(std::vector<glm::vec2> &amps, float phase, std::size_t numberCircles)
+{
+  // 1) track our position in the complex plane as we add circles
+  glm::vec2 position = {0.0f, 0.0f};
+
+  // 2) determine how many circles to draw (if set 0, draw all)
+  int numFreqs = static_cast<int>(numberCircles ? glm::min(numberCircles, amps.size()) : amps.size());
+
+  // 3) iterate over all desired circles
+  for (int circle = 0; circle < numFreqs; circle++)
+  {
+    // 4) for each freq, determine the effect it has on our position (freq * e^i*phase*k)
+    int actualFreq = (circle % 2 == 0) ? circle / 2 : numFreqs - (circle+1) / 2;
+    float displayFreq = (circle % 2 == 0) ? circle / 2 : - (circle + 1) / 2;
+
+    glm::vec2 delta = complex::mult(amps[actualFreq], complex::exp(phase * displayFreq));
+    
+    // 5) calculate the new position
+    glm::vec2 newPos = position + delta;
+
+    // 6) draw a new circle using this information.
+    if (circle != 0) // don't draw the first circle
+    {
+      float radius = glm::length(delta);
+      renderer->DrawCircle(position, glm::vec4(1.0f), radius, 0.025f);
+      renderer->DrawLine(position, newPos, glm::vec4(1.0f), 0.05f);
+    }
+
+    // 7) update the position
+    position = newPos;
+  }
+
+  tracePoints.push_back(position);
 }
 
 }
--- src/art.h
+++ src/art.h
@@ -18,12 +18,17 @@ public:
   void OnUpdate(float timestep);
   void OnResize();
 
+  // display the amplitudes of circles from the origin that draw a point in the complex plane
+  void ShowFFT(std::vector<glm::vec2>& amps, float phase, std::size_t numberCircles = 0);
+
 private:
   Vision::ImGuiRenderer* uiRenderer;
   Vision::Renderer2D* renderer;
   Vision::OrthoCamera* camera;
 
   std::vector<glm::vec2> positions;
+  std::vector<glm::vec2> freqs;
+  std::vector<glm::vec2> tracePoints;
 };
 
 }
